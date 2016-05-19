a344c6c43028bdbb336d925f1e9b2793a4d512c7
John Bubriski <bubriski@gmail.com>
2016-05-19 13:45:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-var http = require('http');
+var fs = require('fs');
+var express = require('express');
+
 var port = process.env.port || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+  fs.readFile('./index.html', 'utf8', function(err, data) {
+    if(!err)res.send(data);
+    else res.status(500).send(err);
+  });
+});
+
+var server = http.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server started');
+  console.log('    Game: http://%s:%s', host, port);
+});
